b38389acd1085678485d77cf24b3931ede53db3a
LeeLawl <laulishuo@gmail.com>
2023-05-23 22:49:20 -0500

    Blank Slate

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css'; // CSS file for styling
+import './App.css';
 
-function App(){
+function App() {
   return (
-    <h1>Hello World!</h1>
-  )
+    <h1>Hello World</h1>
+  );
 }
 
 export default App;
